4029c2a75cabb02eb38be1625393812aa924d3f4
kokou kpadenou <kokou2kpadenou@gmail.com>
2020-05-26 21:19:18 -0400

    tmux script template updated

--- tmux-script-template.sh
+++ tmux-script-template.sh
@@ -18,10 +18,12 @@ if ! tmux has-session -t $session; then
     tmux new-window -t $session:2
     tmux split-window -t $session:2 -v
     tmux send-keys -t $session:1 'vim .' Enter
+    tmux split-window -t $session:1 -c ~/Documents/pratice/tosee
+    tmux send-keys -t $session:1.1 'npm start' Enter
     tmux send-keys -t $session:2.0 'echo my prefere are' Enter
     tmux send-keys -t $session:2.1 'echo Here we are again' Enter
     tmux new-window -t $session:3 mongo
-    tmux split-window -t $session:3 top
+    tmux split-window -t $session:3
     tmux rename-window -t $session:2 BACK-END
     tmux rename-window -t $session:3 DATABASE
 
