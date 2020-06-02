1b93b0d016126b13bb96cc697c628c438b61ebd2
kokou kpadenou <kokou2kpadenou@gmail.com>
2020-06-01 20:43:47 -0400

    deploy 06012020

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Script to deploy my vim dotfiles
+# Script to deploy the dotfiles.
 #
 
 # Plug.vim Installation
@@ -8,19 +8,36 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Links
-ln -s $PWD/ctags ~/.ctags
-ln -s $PWD/vimrc ~/.vimrc
-ln -s $PWD/tmux.conf ~/.tmux.conf
-ln -s $PWD/gitconfig ~/.gitconfig
+# Backup directory
+mkdir ${PWD}/backup
+
+# Symbolic Links Creation
+list="ctags gitconfig gitignore_global vimrc tmux.conf"
+echo "Creation of symbolic links: "
+
+for l in ${list}
+do
+    f="${HOME}/.${l}"
+    t="${PWD}/${l}"
+
+    echo "- ${f}"
+
+    if [ -L $f ]
+    then
+        old_t=`readlink $f`
+        [ ! $old_t = $t ] && { rm $f && ln -s ${t} ${f} }
+    fi
+    # Do this if only the file exist and is not a link
+    [ -f $f ] && { mv $f ${PWD}/backup/.${f}.${DATE} && ln -s ${t} ${f} }
+done
 
 # Create undodir directory
 mkdir ~/.vim/undodir -p
 
-# Message
-echo Please add the following line to your ~/.bash_profile
-echo export TERM=xterm-256color
-
 echo Make sure the following applications are installed 
 sudo dnf install cmake
 sudo dnf install python3-devel
+
+# Message
+echo Please add the following line to your ~/.bash_profile
+echo export TERM=xterm-256color
