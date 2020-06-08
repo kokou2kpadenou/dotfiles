d4772e947fdeffe49d8a695e8bbbeb7a486ebbb1
kokou kpadenou <kokou2kpadenou@gmail.com>
2020-06-07 22:40:25 -0400

    deploy 06072020

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@
 # Plug.vim Installation
 # Download plug.vim and put it in the "autoload" directory.
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Backup directory
 mkdir ${PWD}/backup
@@ -17,29 +17,37 @@ echo "Creation of symbolic links: "
 
 for l in ${list}
 do
-    f="${HOME}/.${l}"
-    t="${PWD}/${l}"
+	f="${HOME}/.${l}"
+	t="${PWD}/${l}"
 
-    echo "- ${f}"
+	echo "- ${f}"
 
-    if [ -L $f ]
-    then
-        old_t=`readlink $f`
-        [ ! $old_t = $t ] && { rm $f && ln -s ${t} ${f} }
-    fi
-    # Do this if only the file exist and is not a link
-    date=`date +%m%d%Y_%H%M%S%N`
-    [ -f $f ] && { mv $f ${PWD}/backup/.${f}.${date} && ln -s ${t} ${f} }
+	if [ -e $f ]; then
+		if [ -L $f ]
+		then
+			old_t=`readlink $f`
+			[ ! $old_t = $t ] && { rm $f && ln -s ${t} ${f}; }
+		else
+			# Do this if only the file exist and is not a link
+			date=`date +%m%d%Y_%H%M%S%N`
+			[ -f $f ] && { mv $f ${PWD}/backup/.${l}.${date} && ln -s ${t} ${f}; }
+		fi
+	else
+		echo "we are here!"
+		ln -s ${t} ${f}
+	fi
 done
 
 # Create undodir directory
 mkdir ~/.vim/undodir -p
 
 # Set terminal Color
-[ ! grep -q "export TERM=xterm-256color" "${HOME}/.bash_profile" ] && echo "export TERM=xterm-256color" >> "${HOME}/.bash_profile"
+grep -q "export TERM=xterm-256color" "${HOME}/.bash_profile" && echo "yes" || echo "export TERM=xterm-256color" >> ${HOME}/.bash_profile
 # Set terminal editor to vi
-[ ! grep -q "set -o vi" "${HOME}/.bashrc" ] && echo "set -o vi" >> "${HOME}/.bashrc"
+grep -q "set -o vi" "${HOME}/.bashrc" && echo "yes" || echo "set -o vi" >> ${HOME}/.bashrc
+# Reload bashrc
+. ~/.bash_profile
 
-echo Make sure the following applications are installed 
-sudo dnf install cmake
-sudo dnf install python3-devel
+echo Installation of cmake and python3-devel
+echo You should have admin privilege
+sudo dnf install cmake python3-devel
