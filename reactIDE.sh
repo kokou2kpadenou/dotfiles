d58129974e5391a1869054f0be933f3edb7df03c
kokou kpadenou <kokou2kpadenou@gmail.com>
2020-06-19 17:31:14 -0400

    react dev env ver 0.1

--- reactIDE.sh
+++ reactIDE.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 ###############################################################################
 #
-#
-#
+#                   ReactJS Development Environment
+#                   Based on Tmux 2.7+ and Vim 8.0+
+#                           -----0000-----
+#                     Write by: Kokou W Kpadenou
+#                        ver0.1 of 06/19/2020
 #
 ###############################################################################
 
-# Read the project directory
+# Get the project directory
 wrkdir=""
 
 while [ "$wrkdir" = "" ]; do
@@ -17,20 +20,19 @@ while [ "$wrkdir" = "" ]; do
     fi
 done
 
-echo $wrkdir
-
 if [ -d $wrkdir ]
 then
     # Get the last directory from the path
     subdir=$(basename $wrkdir)
+    # The name of the session in uppercase
     session=${subdir^^}
+    # Check if the session is in the list of tmux opened sessions
     sessionexist=$(tmux ls | grep $session)
 
     if [ "$sessionexist" = "" ]; then
         tmux new -s $session -d -n DEV -c $wrkdir -x $(tput cols) -y $(tput lines)
         tmux send-keys -t $session:1 'vim .' Enter
         tmux split-window -p 18 -t $session:1 -c $wrkdir
-        #tmux send-keys -t $session:1.1 'npm start' Enter
         tmux split-window -h -t $session:1 -c $wrkdir
         tmux select-pane -t 0 
         tmux new-window -t $session:2 -n TEST -c $wrkdir
