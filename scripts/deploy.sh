2af0b332a3d50f4e7614a822b56f4d796122e3ea
kokou kpadenou <kokou2kpadenou@gmail.com>
2021-02-09 17:09:02 -0500

    deploy script improved

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,40 @@
 # Script to deploy the dotfiles.
 #
 
+# Confirm the dotfiles directoy
+dotfiles_dir="$DOTFILES"
+default_dotfiles_dir="$HOME/.dotfiles"
+
+git_name=""
+git_email=""
+
+
+
+if [ "$dotfiles_dir" = "" ]; then
+
+  read -e -p "Dotfiles directory (defaut: ~/.dotfiles): " dotfiles_dir
+
+  if [ "$dotfiles_dir" = "" ]; then
+    dotfiles_dir=$default_dotfiles_dir
+  fi
+fi
+
+while [ "$git_name" = "" ]; do
+  read -p "Git name: " git_name
+done
+
+while [ "$git_email" = "" ]; do 
+  read -p "Git email: " git_email
+done
+
+
+echo "$dotfiles_dir" "$git_name" "$git_email"
+
+read
+
+# Install GCC and Development Tools
+sh ./development_tools.sh
+
 # installation of eepl repo
 sh ./add_eepl_repo.sh
 
@@ -23,24 +57,30 @@ sh ./ruby.sh
 sh ./node.sh
 
 # NerdFonts DejaVuSansMono installation
-sh ./fonts.sh
+sh ./fonts.sh "$dotfiles_dir"
 
-# Change Gnome Terminal configution manually 
-# - Font, transparency and remove menu bar
-# - theme variant: Dark
-# - uncheck show menubar
-# TODO
+# visual studio code installation
+sh ./vscode.sh
 
+# chrome installation
+sh ./chrome.sh
 
 # Symbolic Links Creation
 
-ln -s -b ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s -b ~/${dotfiles_dir}/gitconfig ~/.gitconfig
 
-ln -s -b ~/.dotfiles/gitignore_global ~/.gitignore_global
+ln -s -b ~/${dotfiles_dir}/gitignore_global ~/.gitignore_global
 
-ln -s -b ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s -b ~/${dotfiles_dir}/tmux.conf ~/.tmux.conf
 
-ln -s -b ~/.dotfiles/nvim ~/.config/nvim
+ln -s -b ~/${dotfiles_dir}/nvim ~/.config/nvim
+
+#
+# Set Git - name and email
+#
+
+git config --global user.name "$git_name"
+git config --global user.email "$git_email"
 
 #
 # Set Environnement Variables
@@ -66,6 +106,16 @@ sudo dnf autoremove
 
 sh ./nodejsENOSPCerrorFix.sh
 
+# Change Gnome Terminal configution manually 
+# - Font, transparency and remove menu bar
+# - theme variant: Dark
+# - uncheck show menubar
+# TODO
+
+echo "Before continue, please stp setup GNOME Terminal"
+echo "Font: DejaVuSansMono, Transparency: ~12%, Remove memu bar, Theme: dark"
+echo "If ready press enter to continue."
+read
 
 #
 # End
