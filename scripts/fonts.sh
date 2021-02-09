6365f576f8321f663f4da23e704d312ada5b3794
kokou kpadenou <kokou2kpadenou@gmail.com>
2021-02-09 17:08:18 -0500

    fonts script improved with file to install dir font as arg

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
-FONTS_INSTALL_PATH="/usr/share/fonts"
-FONTS_TO_INSTALL_DIR="$HOME/.dotfiles/fonts/DejaVuSansMono"
 
-sudo mv "$FONTS_TO_INSTALL_DIR" "$FONTS_INSTALL_PATH"
+dotfiles_dir_fonts="$1"
+default_dotfiles_dir="$HOME/.dotfiles"
+
+if [ "$1" = "" ]; then 
+  read -e -p "Dotfiles dir (defaut: ~/.dotfiles): " dotfiles_dir_fonts
+
+  if [$dotfiles_dir_fonts = ""]; then
+    dotfiles_dir_fonts="$default_dotfiles_dir"
+  fi
+fi
+
+
+FONTS_INSTALL_PATH="/usr/share/fonts"
+FONTS_DIR="${dotfiles_dir_fonts}/fonts/DejaVuSansMono"
+
+echo "$FONTS_DIR"
+
+sudo mv "$FONTS_DIR" "$FONTS_INSTALL_PATH"
 
 sudo fc-cache -f -v
