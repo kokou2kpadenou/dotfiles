95d2b56d9a2c325817e30e65692b8d079098dfb1
kokou kpadenou <kokou2kpadenou@gmail.com>
2021-02-09 18:12:01 -0500

    use of sudo for permisson

--- scripts/chrome.sh
+++ scripts/chrome.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-sudo cat << EOF > /etc/yum.repos.d/google-chrome.repo
+sudo bash -c 'cat << EOF > /etc/yum.repos.d/google-chrome.repo1
 [google-chrome]
 name=google-chrome
 baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
 enabled=1
 gpgcheck=1
 gpgkey=https://dl.google.com/linux/linux_signing_key.pub
-EOF
+EOF'
 
-sudo dnf install google-chrome-stable -y
+# sudo dnf install google-chrome-stable -y
