277d8489f61cc340367db9ccfee8cd9d5c2e4530
Kokou Kpadenou <kkokou@gmail.com>
2021-02-11 11:57:48 -0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,4 +8,4 @@ if [ "$entry" = "" ]; then
   entry=".dotfiles"
 fi
 
-git clone https://github.com/kokou2kpadenou/dotfiles.git "$entry" && cd $_ && sh scripts/deploy.sh
+git clone https://github.com/kokou2kpadenou/dotfiles.git "$entry" && cd $_
