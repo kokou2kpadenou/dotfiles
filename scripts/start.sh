3bde0a378deae6aef8d522784562e2eff30f0f03
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2021-03-15 20:33:25 -0400

    updated start to run the appropriate script for centos or archlinux

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,10 +2,33 @@
 
 entry=""
 
-read -p "Clone name (default: .dotfiles): " entry
+read -p "Dotfiles folder name (default: .dotfiles): " entry
 
 if [ "$entry" = "" ]; then 
   entry=".dotfiles"
 fi
 
-git clone https://github.com/kokou2kpadenou/dotfiles.git "$entry" && sh ${entry}/scripts/deploy.sh
+
+# OS Selection
+os=""
+
+PS3='Please select the Operation System: '
+options=("CentOS" "Archlinux")
+select opt in "${options[@]}"
+do
+  case $opt in
+    "CentOS")
+      os="centos"
+      break
+      ;;
+    "Archlinux")
+      os="archlinux"
+      break
+      ;;
+    *) echo "invalid option $REPLY";;
+  esac
+done
+
+clear
+
+git clone https://github.com/kokou2kpadenou/dotfiles.git "$entry" && sh ${entry}/scripts/${os}/post_install.sh
