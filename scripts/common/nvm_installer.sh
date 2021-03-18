46d1a4c25003b2b776661da74ab446628d54f1b6
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2021-03-18 01:23:53 -0400

    fixed condition synthax

--- scripts/common/nvm_installer.sh
+++ scripts/common/nvm_installer.sh
@@ -3,8 +3,8 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh"] && \. "$NVM_DIR/nvm.sh"  # this loads nvm
-[ -s "$NVM_DIR/bash_completion"] && \. "$NVM_DIR/bash_completion"  # this loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm inatall lts/erbium
 nvm use lts/erbium
