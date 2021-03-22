42438648659d6ccf5377f1c36637e22a7345055a
kokou kpadenou <kokou2kpadenou@gmail>
2021-03-22 09:54:31 -0400

    remove the symbolic link of gitconfig

--- scripts/archlinux/post_install.sh
+++ scripts/archlinux/post_install.sh
@@ -172,7 +172,6 @@ ln -s -b ${dotfiles_dir}/rofi ~/.config/rofi
 ln -s -b ${dotfiles_dir}/tmux.conf ~/.tmux.conf
 ln -s -b ${dotfiles_dir}/.xinitrc ~/.xinitrc
 ln -s -b ${dotfiles_dir}/.Xresources ~/.Xresources
-ln -s -b ${dotfiles_dir}/gitconfig ~/.gitconfig
 ln -s -b ${dotfiles_dir}/gitignore_global ~/.gitignore_global
 
 # Git settings
