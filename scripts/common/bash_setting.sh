a8efea840b67fb0baded322cef57d79fcf1b3898
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2021-03-28 19:14:33 -0400

    integrated .bash_aliases to .bashrc

--- scripts/common/bash_setting.sh
+++ scripts/common/bash_setting.sh
@@ -1,2 +1,10 @@
 # Set terminal editor to vi
 echo "set -o vi" >> ${HOME}/.bashrc
+
+# Aliases
+cat <<EOT >> ${HOME}/.bashrc
+# Aliases
+if [ -e $HOME/.bash_aliases ]; then
+    source $HOME/.bash_aliases
+fi
+EOT
