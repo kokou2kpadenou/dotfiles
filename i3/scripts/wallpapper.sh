f1f68a32f74f8b34aee013b4005c0dc5897d2184
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2021-06-17 22:02:30 -0400

    new script to set wallpaper
    This script set only once the wallpaper when i3 is started. It put in i3
    config

---
+++ i3/scripts/wallpapper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+WALLPAPERS=~/Pictures/wallpapers;
+DEFAULT_WALLPAPERS=~/.dotfiles/wallpapers;
+
+# if google image is downloaded then CURRENT_WALLPAPERS=WALLPAPERS
+# Else CURRENT_WALLPAPERS=DEFAULT_WALLPAPERS
+JPG=$(find "$WALLPAPERS" -name *.jpg|head -n1)
+[ ! -z "$JPG" ] && CURRENT_WALLPAPERS="${WALLPAPERS}/earthview.withgoogle.com/download/*" || CURRENT_WALLPAPERS="${DEFAULT_WALLPAPERS}/*"
+
+# Apply the wallpaper
+feh --bg-scale --randomize $CURRENT_WALLPAPERS
