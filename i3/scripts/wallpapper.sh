f96b9a4e9f9d1e693d769f7bfb48932b9eca56bd
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2021-07-30 17:19:19 -0400

    improve i3 wallpaper script
    increase images
    download only the image not the whole directory

--- i3/scripts/wallpapper.sh
+++ i3/scripts/wallpapper.sh
@@ -6,7 +6,7 @@ DEFAULT_WALLPAPERS=~/.dotfiles/wallpapers;
 # if google image is downloaded then CURRENT_WALLPAPERS=WALLPAPERS
 # Else CURRENT_WALLPAPERS=DEFAULT_WALLPAPERS
 JPG=$(find "$WALLPAPERS" -name *.jpg|head -n1)
-[ ! -z "$JPG" ] && CURRENT_WALLPAPERS="${WALLPAPERS}/earthview.withgoogle.com/download/*" || CURRENT_WALLPAPERS="${DEFAULT_WALLPAPERS}/*"
+[ ! -z "$JPG" ] && CURRENT_WALLPAPERS="${WALLPAPERS}/*" || CURRENT_WALLPAPERS="${DEFAULT_WALLPAPERS}/*"
 
 # Apply the wallpaper
 feh --bg-scale --randomize $CURRENT_WALLPAPERS
