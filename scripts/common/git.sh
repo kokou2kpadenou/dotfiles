a3a51c9626f86fbaca847ea0af6773ebc602032e
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2021-08-10 16:17:46 -0400

    change git script to use gitconfig.local

--- scripts/common/git.sh
+++ scripts/common/git.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-git config --global user.name "$1"
-git config --global user.email "$2"
-
-git config --global color.status auto
-git config --global color.branch auto
-git config --global color.interactive auto
-git config --global color.diff auto
-
-git config --global merge.conflictstyle diff3
-
-git config --global core.editor "$3"
-
+git config --file ~/.gitconfig.local user.name "$1"
+git config --file ~/.gitconfig.local user.email "$2"
