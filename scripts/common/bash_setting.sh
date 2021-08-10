4b24cd8883d6fd3b0116a9c69d95da0205648936
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2021-08-10 16:21:03 -0400

    update script

--- scripts/common/bash_setting.sh
+++ scripts/common/bash_setting.sh
@@ -3,6 +3,9 @@
 # Set terminal editor to vi
 echo "set -o vi" >> ${HOME}/.bashrc
 
+echo "export DOTFILES=${$1}" >> ${HOME}/.bashrc
+echo "export EDITOR=vim" >> ${HOME}/.bashrc
+
 # Aliases
 cat <<EOT >> ${HOME}/.bashrc
 # Aliases
