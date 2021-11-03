e2924dfa3a042d6111ea130b20b5ccd6f4a0fb42
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2021-11-02 22:08:19 -0400

    change separators setting

--- settings/w_o_nvimlua/.config/nvim/lua/lualineconfig.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/lualineconfig.lua
@@ -5,8 +5,6 @@ require'lualine'.setup {
     theme = 'material-nvim',
     section_separators = { left = '', right = ''},
     component_separators = { left = '', right = ''},
-    -- component_separators = {'', ''},
-    -- section_separators = {'', ''},
     disabled_filetypes = {}
   },
   sections = {
