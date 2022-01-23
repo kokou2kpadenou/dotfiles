43a9af47d5dce186fa25a738d4f01a3b2b624cb4
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2022-01-22 22:49:20 -0500

    change lsp confuguration

---
+++ settings/w_o_nvimlua/.config/nvim/lua/lsp.lua
@@ -0,0 +1,223 @@
+-- Diagnostic keymaps
+vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap = true, silent = true })
+
+-- LSP settings
+local lspconfig = require 'lspconfig'
+
+local on_attach = function(_, bufnr)
+  local opts = { noremap = true, silent = true }
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+end
+
+-- nvim-cmp supports additional completion capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+-- Enable the following language servers
+local servers = { 'jsonls', 'cssls', 'html', 'tailwindcss' }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- Example custom server
+-- Make runtime files discoverable to the server
+-- local runtime_path = vim.split(package.path, ';')
+-- table.insert(runtime_path, 'lua/?.lua')
+-- table.insert(runtime_path, 'lua/?/init.lua')
+
+-- lspconfig.sumneko_lua.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   settings = {
+--     Lua = {
+--       runtime = {
+--         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+--         version = 'LuaJIT',
+--         -- Setup your lua path
+--         path = runtime_path,
+--       },
+--       diagnostics = {
+--         -- Get the language server to recognize the `vim` global
+--         globals = { 'vim' },
+--       },
+--       workspace = {
+--         -- Make the server aware of Neovim runtime files
+--         library = vim.api.nvim_get_runtime_file('', true),
+--       },
+--       -- Do not send telemetry data containing a randomized but unique identifier
+--       telemetry = {
+--         enable = false,
+--       },
+--     },
+--   },
+-- }
+
+-- tsserver - JavaScript lsp config
+require('lspconfig').tsserver.setup {
+  on_attach = function(client)
+    -- Show functon signature
+    require('lsp_signature').on_attach {
+      -- bind = true, -- This is mandatory, otherwise border config won't get registered.
+      -- doc_lines = 2, -- will show 2 lines of comment/doc(if there are more than 2 lines in doc, will be truncated)
+      -- -- set to 0 if you DO NOT want any API comments be shown
+      -- -- This setting only take effect in insert mode, it does not affect signature help in normal
+      -- -- mode, 10 by default
+
+      -- floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
+      -- hint_enable = true,
+      -- hint_prefix = "🌟 ",
+      -- hint_scheme = "String",
+      -- use_lspsaga = false,
+      -- hi_parameter = "PmenuSel", -- hl-search
+      -- max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
+      -- -- to view the hiding contents
+      -- max_width = 120, -- max_width of signature floating_window, line will be wrapped if exceed max_width
+      -- handler_opts = {
+      --   border = "single", -- double, single, shadow, none
+      -- },
+      -- extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
+
+        debug = false, -- set to true to enable debug logging
+        log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log", -- log dir when debug is on
+        -- default is  ~/.cache/nvim/lsp_signature.log
+        verbose = false, -- show debug line number
+
+        bind = true, -- This is mandatory, otherwise border config won't get registered.
+                     -- If you want to hook lspsaga or other signature handler, pls set to false
+        doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
+                       -- set to 0 if you DO NOT want any API comments be shown
+                       -- This setting only take effect in insert mode, it does not affect signature help in normal
+                       -- mode, 10 by default
+
+        floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
+
+        floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
+        -- will set to true when fully tested, set to false will use whichever side has more space
+        -- this setting will be helpful if you do not want the PUM and floating win overlap
+
+        floating_window_off_x = 1, -- adjust float windows x position.
+        floating_window_off_y = 1, -- adjust float windows y position.
+
+
+        fix_pos = false,  -- set to true, the floating window will not auto-close until finish all parameters
+        hint_enable = true, -- virtual hint enable
+        hint_prefix = "🐼 ",  -- Panda for parameter
+        hint_scheme = "String",
+        hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
+        max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
+                         -- to view the hiding contents
+        max_width = 80, -- max_width of signature floating_window, line will be wrapped if exceed max_width
+        handler_opts = {
+          border = "rounded"   -- double, rounded, single, shadow, none
+        },
+
+        always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
+
+        auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
+        extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
+        zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
+
+        padding = '', -- character to pad on left and right of signature can be ' ', or '|'  etc
+
+        transparency = nil, -- disabled by default, allow floating win transparent value 1~100
+        shadow_blend = 36, -- if you using shadow as border use this set the opacity
+        shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
+        timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
+        toggle_key = nil -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+
+    }
+
+    -- Imort support
+    local ts_utils = require "nvim-lsp-ts-utils"
+
+    -- defaults
+    ts_utils.setup {
+      debug = false,
+      disable_commands = false,
+      enable_import_on_completion = false,
+      import_all_timeout = 5000, -- ms
+
+      -- eslint
+      -- using eslint lsp directly now, see below
+      eslint_enable_code_actions = false,
+      eslint_enable_disable_comments = false,
+      eslint_bin = "eslint",
+      eslint_config_fallback = nil,
+      eslint_enable_diagnostics = false,
+
+      -- TODO: try out update imports on file move
+      update_imports_on_move = true,
+      require_confirmation_on_move = false,
+      watch_dir = nil,
+    }
+    -- required to fix code action ranges
+    ts_utils.setup_client(client)
+
+    -- disable tsserver formatting
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+  end,
+  capabilities = capabilities,
+}
+
+lspconfig.eslint.setup {
+  on_attach = function(client)
+    -- neovim's LSP client does not currently support dynamic capabilities registration, so we need to set
+    -- the resolved capabilities of the eslint server ourselves!
+    client.resolved_capabilities.document_formatting = true
+  end,
+  settings = {
+    format = { enable = true },
+  },
+}
+
+
+-- Change diagnostic symbols in the sign column (gutter)
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- LSP Enable diagnostics
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = false,
+  underline = true,
+  signs = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
+local border_style = {
+  { "╭", "FloatBorder" },
+  { "─", "FloatBorder" },
+  { "╮", "FloatBorder" },
+  { "│", "FloatBorder" },
+  { "╯", "FloatBorder" },
+  { "─", "FloatBorder" },
+  { "╰", "FloatBorder" },
+  { "│", "FloatBorder" },
+}
+
+local pop_opts = { border = border_style }
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, pop_opts)
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, pop_opts)
