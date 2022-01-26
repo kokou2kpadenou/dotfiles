c37857db0b44ace600a14966f8d2086aef425693
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2022-01-25 23:17:24 -0500

    add keymap to toggle material style

--- settings/w_o_nvimlua/.config/nvim/lua/materialconfig.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/materialconfig.lua
@@ -1,39 +1,6 @@
 vim.g.material_style = "darker"
 
 require('material').setup({
-
-	-- contrast = true, -- Enable contrast for sidebars, floating windows and popup menus like Nvim-Tree
-	-- borders = false, -- Enable borders between verticaly split windows
-
-	-- popup_menu = "dark", -- Popup menu style ( can be: 'dark', 'light', 'colorful' or 'stealth' )
-
-	-- italics = {
-	-- 	comments = false, -- Enable italic comments
-	-- 	keywords = false, -- Enable italic keywords
-	-- 	functions = false, -- Enable italic functions
-	-- 	strings = false, -- Enable italic strings
-	-- 	variables = false -- Enable italic variables
-	-- },
-
-	-- contrast_windows = { -- Specify which windows get the contrasted (darker) background
-	-- 	"terminal", -- Darker terminal background
-	-- 	"packer", -- Darker packer background
-	-- 	"qf" -- Darker qf list background
-	-- },
-
-	-- text_contrast = {
-	-- 	lighter = false, -- Enable higher contrast text for lighter style
-	-- 	darker = false -- Enable higher contrast text for darker style
-	-- },
-
-	-- disable = {
-	-- 	background = true, -- Prevent the theme from setting the background (NeoVim then uses your teminal background)
-	-- 	term_colors = false, -- Prevent the theme from setting terminal colors
-	-- 	eob_lines = false -- Hide the end-of-buffer lines
-	-- },
-
-	-- custom_highlights = {} -- Overwrite highlights with your own
-
   
 		contrast = {
 			sidebars = true,
@@ -64,3 +31,6 @@ require('material').setup({
 })
 
 vim.cmd 'colorscheme material'
+
+--Lua:
+vim.api.nvim_set_keymap('n', '<leader>mm', [[<Cmd>lua require('material.functions').toggle_style()<CR>]], { noremap = true, silent = true })
