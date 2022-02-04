c5326ef8f6e7f93a658a04f3f6fc4c55488a312a
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2022-02-03 22:02:12 -0500

    set theme to dropdown and try fzf

--- settings/w_o_nvimlua/.config/nvim/lua/telescopeconfig.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/telescopeconfig.lua
@@ -1,6 +1,23 @@
 require('telescope').setup {
     defaults = {
-        file_ignore_patterns = { "node_modules", ".git" },
-        color_devicons = true
-    }
+      file_ignore_patterns = { "node_modules", ".git" },
+      color_devicons = true
+    },
+
+    pickers = {
+      find_files = {
+        theme = "dropdown",
+      }
+    },
+
+    -- extensions = {
+    --   fzf = {
+    --     fuzzy = true,
+    --     override_generic_sorter = true,
+    --     override_file_sorter = true,
+    --     case_mode = "smart_case",
+    --   }
+    -- }
 }
+
+-- require('telescope').load_extension('fzf')
