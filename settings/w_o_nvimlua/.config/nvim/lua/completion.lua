641c1ee6b53aab8886983c40ab27e0f5ba59f5ff
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2022-02-03 22:07:42 -0500

    change cmp settings

--- settings/w_o_nvimlua/.config/nvim/lua/completion.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/completion.lua
@@ -2,8 +2,34 @@
 
 local cmp = require'cmp'
 
-local lspkind = require "lspkind"
-lspkind.init()
+
+local kind_icons = {
+  Text = "",
+  Method = "",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "ﴯ",
+  Interface = "",
+  Module = "",
+  Property = "ﰠ",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = ""
+}
 
 cmp.setup({
   snippet = {
@@ -13,27 +39,33 @@ cmp.setup({
   },
 
   formatting = {
-    format = lspkind.cmp_format {
-      with_text = true,
-      menu = {
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      -- Source
+      vim_item.menu = ({
         buffer = '[buf]',
+        nvim_lua = '[LUA]',
         nvim_lsp = '[LSP]',
-        nvim_lua = '[api]',
         path = '[path]',
         luasnip = '[snip]',
-        cmdline = '[CMD]',
         spell = '[spell]',
         treesitter = '[synx]'
-      },
-    },
+      })[entry.source.name]
+      return vim_item
+    end
   },
 
   mapping = {
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   },
 
   sources = {
@@ -54,12 +86,3 @@ cmp.setup.cmdline('/', {
     { name = 'buffer', keyword_length = 3 }
   }
 })
-
--- Use cmdline & path source for ':'.
--- cmp.setup.cmdline(':', {
---   sources = cmp.config.sources({
---     { name = 'path' }
---   }, {
---     { name = 'cmdline', keyword_length = 4 }
---   })
--- })
