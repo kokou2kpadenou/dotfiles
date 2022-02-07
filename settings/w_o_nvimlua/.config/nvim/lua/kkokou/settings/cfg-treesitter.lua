176ad745272e63bea7cb80f51f3420a5192417f5
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2022-02-06 22:37:54 -0500

    add go to treesitter config

--- settings/w_o_nvimlua/.config/nvim/lua/kkokou/settings/cfg-treesitter.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/kkokou/settings/cfg-treesitter.lua
@@ -13,6 +13,7 @@ require('nvim-treesitter.configs').setup {
     'lua',
     'jsdoc',
     'scss',
+    'go',
   }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   -- ignore_install = { "javascript" }, -- List of parsers to ignore installing
   highlight = {
