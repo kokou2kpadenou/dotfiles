cbb40d4a9db3b94b39040716c8a11c192ea8d78b
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2022-04-28 11:24:52 -0400

    clean telescope setting and add plugins

--- settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/settings/cfg-telescope.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/settings/cfg-telescope.lua
@@ -3,21 +3,7 @@ require('telescope').setup {
     file_ignore_patterns = { 'node_modules', '.git' },
     color_devicons = true,
   },
-
-  pickers = {
-    find_files = {
-      theme = 'dropdown',
-    },
-  },
-
-  -- extensions = {
-  --   fzf = {
-  --     fuzzy = true,
-  --     override_generic_sorter = true,
-  --     override_file_sorter = true,
-  --     case_mode = "smart_case",
-  --   }
-  -- }
 }
 
--- require('telescope').load_extension('fzf')
+require('telescope').load_extension 'fzf'
+require('telescope').load_extension 'file_browser'
