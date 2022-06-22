06525f4115317bf9dca6fe0a56756db9aff9cc40
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2022-06-22 18:30:32 -0400

    set different winbar for packer, help, checkhealth and netrw

--- settings/w_o_nvimlua/.config/nvim/after/plugin/init.lua
+++ settings/w_o_nvimlua/.config/nvim/after/plugin/init.lua
@@ -1,4 +1,9 @@
 -- Activate global statusline
 vim.o.laststatus = 3
 
-vim.o.winbar = "%=%m %f"
+vim.o.winbar = '%=%m %f'
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'netrw', 'checkhealth', 'packer', 'help' },
+  command = 'setlocal winbar=%=%y',
+})
