69472e5478663837a224540f78a9b3bd33e2a3b9
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2022-06-28 08:49:55 -0400

    add undotree and diff winbar exception setting

--- settings/w_o_nvimlua/.config/nvim/after/plugin/init.lua
+++ settings/w_o_nvimlua/.config/nvim/after/plugin/init.lua
@@ -4,6 +4,6 @@ vim.o.laststatus = 3
 vim.o.winbar = '%=%m %f'
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'netrw', 'checkhealth', 'packer', 'help' },
+  pattern = { 'netrw', 'checkhealth', 'packer', 'help', 'undotree', 'diff' },
   command = 'setlocal winbar=%=%y',
 })
