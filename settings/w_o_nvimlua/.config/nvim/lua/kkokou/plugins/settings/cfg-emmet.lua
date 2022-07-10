5b53807be471f5c862ee06ffded19aafab8a6de4
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2022-07-09 21:38:01 -0400

    add fixme message

--- settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/settings/cfg-emmet.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/settings/cfg-emmet.lua
@@ -6,7 +6,7 @@ local g = vim.g -- a table to access global variables
 cmd [[autocmd FileType astro,html,css,javascript,javascriptreact EmmetInstall]]
 
 g.user_emmet_install_global = 0
-g.user_emmet_leader_key = ','
+g.user_emmet_leader_key = ',' -- FIXME: key would not change, still using the default C-Y
 g.user_emmet_settings = {
   javascript = {
     extends = 'jsx',
