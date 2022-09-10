d3b3ad6f3b5b9f4a087ec62f183b5e1fd8f26bbf
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2022-09-10 17:57:23 -0400

    set fzf for telescope

--- settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/settings/cfg-telescope.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/settings/cfg-telescope.lua
@@ -3,6 +3,14 @@ require('telescope').setup {
     file_ignore_patterns = { 'node_modules', '.git' },
     color_devicons = true,
   },
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = 'smart_case',
+    }
+  }
 }
 
 require('telescope').load_extension 'fzf'
