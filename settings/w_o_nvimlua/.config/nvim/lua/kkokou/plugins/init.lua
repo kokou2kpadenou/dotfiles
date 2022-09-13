0ce2624f7397d4b60079dd6bd02beae66239115c
kokou kpadenou <kokou2kpadenou@gmail.com>
2022-09-12 20:52:55 -0400

    Add interaction with databases

--- settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/init.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/init.lua
@@ -230,7 +230,32 @@ return packer.startup(function(use)
       -- {'ibhagwan/fzf-lua'},
     },
     config = function()
-      require('kkokou.plugins.settings.cfg-neoclip')
+      require 'kkokou.plugins.settings.cfg-neoclip'
+    end,
+  }
+
+  -- Interaction with Databases
+  use {
+    'kristijanhusak/vim-dadbod-ui',
+    disable = false,
+    requires = {
+      'tpope/vim-dadbod',
+      'kristijanhusak/vim-dadbod-completion',
+      'hrsh7th/nvim-cmp',
+    },
+    config = function()
+      -- TODO: Move later to a file
+      vim.g.db_ui_save_location = '~/Documents/.database/db_ui'
+
+      local function enable_completion()
+        require('cmp').setup.buffer({ sources = { { name = 'vim-dadbod-completion' } } })
+      end
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'sql', 'mysql', 'plsql' },
+        callback = enable_completion
+      })
+
     end,
   }
 
