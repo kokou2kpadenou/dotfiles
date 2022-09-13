3709a56f4dcbfd9ff5cb766ead6b4ef0d3a34219
kokou kpadenou <kokou2kpadenou@gmail.com>
2022-09-12 20:47:59 -0400

    Correct tokyonight setup

--- settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/settings/cfg-tokyonight.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/settings/cfg-tokyonight.lua
@@ -1,32 +1,23 @@
--- Example config in Lua
---[[ vim.g.tokyonight_style = 'storm' ]]
---[[ vim.g.tokyonight_transparent = true ]]
---[[ vim.g.tokyonight_italic_functions = true ]]
---[[ vim.g.tokyonight_sidebars = { 'qf', 'vista_kind', 'terminal', 'packer' } ]]
---[[ vim.g.tokyonight_dark_float = false ]]
-
--- Change the "hint" color to the "orange" color, and make the "error" color bright red
--- vim.g.tokyonight_colors = { hint = 'orange', error = '#ff0000' }
-require("tokyonight").setup({
+require('tokyonight').setup {
   -- your configuration comes here
   -- or leave it empty to use the default settings
-  style = "storm", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+  style = 'storm', -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
   transparent = true, -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
   styles = {
     -- Style to be applied to different syntax groups
     -- Value is any valid attr-list value `:help attr-list`
-    comments = "italic",
-    keywords = "italic",
-    functions = "NONE",
-    variables = "NONE",
+    comments = 'italic',
+    keywords = 'italic',
+    functions = 'NONE',
+    variables = 'NONE',
     -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "dark", -- style for sidebars, see below
-    floats = "dark", -- style for floating windows
+    sidebars = 'transparent', -- style for sidebars, see below
+    floats = 'dark', -- style for floating windows
   },
-  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  sidebars = { 'qf', 'help' }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
   day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-  hide_inactive_statusline = true, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
   dim_inactive = false, -- dims inactive windows
   lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 
@@ -40,7 +31,7 @@ require("tokyonight").setup({
   ---@param highlights Highlights
   ---@param colors ColorScheme
   on_highlights = function(highlights, colors) end,
-})
+}
 
 -- Load the colorscheme
 vim.cmd [[colorscheme tokyonight]]
