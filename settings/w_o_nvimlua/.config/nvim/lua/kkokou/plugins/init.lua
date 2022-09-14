58899d14876c44d087db1bb4e700a509003df139
kokou kpadenou <kokou2kpadenou@gmail.com>
2022-09-14 18:45:37 -0400

    Extract dadbod-ui setting to a file

--- settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/init.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/init.lua
@@ -234,18 +234,7 @@ return packer.startup(function(use)
       'hrsh7th/nvim-cmp',
     },
     config = function()
-      -- TODO: Move later to a file
-      vim.g.db_ui_save_location = '~/Documents/.database/db_ui'
-
-      local function enable_completion()
-        require('cmp').setup.buffer({ sources = { { name = 'vim-dadbod-completion' } } })
-      end
-
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = { 'sql', 'mysql', 'plsql' },
-        callback = enable_completion
-      })
-
+      require 'kkokou.plugins.settings.cfg-vim-dadbod-ui'
     end,
   }
 
