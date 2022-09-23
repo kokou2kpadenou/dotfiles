1f0e381132b4c828c32cc9aa92f1f5b2403ffb3f
kokou kpadenou <kokou2kpadenou@gmail.com>
2022-09-22 22:15:11 -0400

    Improve and clear luasnip settings

--- settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/settings/cfg-luasnip.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/settings/cfg-luasnip.lua
@@ -1,38 +1,24 @@
---
-
 local ls = require 'luasnip'
 local types = require 'luasnip.util.types'
 
 ls.config.set_config {
   history = true,
-  -- Update more often, :h events for more info.
   updateevents = 'TextChanged,TextChangedI',
-  -- Snippets aren't automatically removed if their text is deleted.
-  -- `delete_check_events` determines on which events (:h events) a check for
-  -- deleted snippets is performed.
-  -- This can be especially useful when `history` is enabled.
   delete_check_events = 'TextChanged',
   ext_opts = {
     [types.choiceNode] = {
       active = {
-        virt_text = { { 'choiceNode', 'Comment' } },
+        virt_text = { { '●', 'DiagnosticError' } },
+      },
+    },
+    [types.insertNode] = {
+      active = {
+        virt_text = { { '●', 'DiagnosticHint' } },
       },
     },
   },
-  -- treesitter-hl has 100, use something higher (default is 200).
   ext_base_prio = 300,
-  -- minimal increase in priority.
-  -- ext_prio_increase = 1,
   enable_autosnippets = true,
-  -- mapping for cutting selected text so it's usable as SELECT_DEDENT,
-  -- SELECT_RAW or TM_SELECTED_TEXT (mapped via xmap).
-  -- store_selection_keys = "<Tab>",
-  -- luasnip uses this function to get the currently active filetype. This
-  -- is the (rather uninteresting) default, but it's possible to use
-  -- eg. treesitter for getting the current filetype by setting ft_func to
-  -- require("luasnip.extras.filetype_functions").from_cursor (requires
-  -- `nvim-treesitter/nvim-treesitter`). This allows correctly resolving
-  -- the current filetype in eg. a markdown-code block or `vim.cmd()`.
   ft_func = function()
     return vim.split(vim.bo.filetype, '.', true)
   end,
@@ -41,59 +27,20 @@ ls.config.set_config {
 -- Load friendly-snippets
 require('luasnip.loaders.from_vscode').lazy_load {
   paths = { vim.fn.stdpath 'data' .. '/site/pack/packer/start/friendly-snippets' },
-} -- You can pass { paths = "./my-snippets/"} as well
+}
 
 -- Mapping
-local function prequire(...)
-  local status, lib = pcall(require, ...)
-  if status then
-    return lib
-  end
-  return nil
-end
+vim.cmd [[
+  " press <Tab> to expand or jump in a snippet. These can also be mapped separately
+  " via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
+  imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
+  " -1 for jumping backwards.
+  inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
 
-local luasnip = prequire 'luasnip'
-local cmp = prequire 'cmp'
+  snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
+  snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
 
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-  local col = vim.fn.col '.' - 1
-  if col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' then
-    return true
-  else
-    return false
-  end
-end
-
-_G.tab_complete = function()
-  if cmp and cmp.visible() then
-    cmp.select_next_item()
-  elseif luasnip and luasnip.expand_or_jumpable() then
-    return t '<Plug>luasnip-expand-or-jump'
-  elseif check_back_space() then
-    return t '<Tab>'
-  else
-    cmp.complete()
-  end
-  return ''
-end
-_G.s_tab_complete = function()
-  if cmp and cmp.visible() then
-    cmp.select_prev_item()
-  elseif luasnip and luasnip.jumpable(-1) then
-    return t '<Plug>luasnip-jump-prev'
-  else
-    return t '<S-Tab>'
-  end
-  return ''
-end
-
-vim.api.nvim_set_keymap('i', '<C-F>', 'v:lua.tab_complete()', { expr = true })
-vim.api.nvim_set_keymap('s', '<C-F>', 'v:lua.tab_complete()', { expr = true })
-vim.api.nvim_set_keymap('i', '<C-B>', 'v:lua.s_tab_complete()', { expr = true })
-vim.api.nvim_set_keymap('s', '<C-B>', 'v:lua.s_tab_complete()', { expr = true })
-vim.api.nvim_set_keymap('i', '<C-E>', '<Plug>luasnip-next-choice', {})
-vim.api.nvim_set_keymap('s', '<C-E>', '<Plug>luasnip-next-choice', {})
+  " For changing choices in choiceNodes (not strictly necessary for a basic setup).
+  imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+  smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+]]
