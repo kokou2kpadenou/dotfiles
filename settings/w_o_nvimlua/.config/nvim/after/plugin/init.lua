3bdb199a786c6bcbdbe9e74a33d2af4c01af4d98
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2022-10-24 13:18:32 -0400

    remove winbar config, will add to lualine setup

--- settings/w_o_nvimlua/.config/nvim/after/plugin/init.lua
+++
@@ -1,9 +0,0 @@
--- Activate global statusline
-vim.o.laststatus = 3
-
-vim.o.winbar = '%=%m %f'
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'netrw', 'checkhealth', 'packer', 'help', 'undotree', 'diff', 'dbui' },
-  command = 'setlocal winbar=%=%y',
-})
