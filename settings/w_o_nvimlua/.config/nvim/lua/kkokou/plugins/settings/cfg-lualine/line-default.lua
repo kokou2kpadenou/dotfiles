eaabd87ea26c41d3b88c0c778c368d49bc17565f
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2022-10-29 19:29:12 -0400

    Reconfigurate default lualine

--- settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/settings/cfg-lualine/line-default.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/settings/cfg-lualine/line-default.lua
@@ -1,61 +1,41 @@
-local function window()
-  return vim.api.nvim_win_get_number(0)
-end
-
-local custom_fname = require('lualine.components.filename'):extend()
-local highlight = require 'lualine.highlight'
-local default_status_colors = { saved = '#228B22', modified = '#C70039' }
-
-function custom_fname:init(options)
-  custom_fname.super.init(self, options)
-  self.status_colors = {
-    saved = highlight.create_component_highlight_group(
-      { bg = default_status_colors.saved },
-      'filename_status_saved',
-      self.options
-    ),
-    modified = highlight.create_component_highlight_group(
-      { bg = default_status_colors.modified },
-      'filename_status_modified',
-      self.options
-    ),
-  }
-  if self.options.color == nil then
-    self.options.color = ''
-  end
-end
-
-function custom_fname:update_status()
-  local data = custom_fname.super.update_status(self)
-  data = highlight.component_format_highlight(
-    vim.bo.modified and self.status_colors.modified or self.status_colors.saved
-  ) .. data
-  return data
-end
-
 return {
   options = {
     icons_enabled = true,
-    section_separators = { left = '', right = '' },
-    component_separators = { left = '', right = '' },
+    theme = 'tokyonight',
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
   },
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch' },
-    -- lualine_c = {'filename'},
-    lualine_c = { window, custom_fname },
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_c = {
+      {
+        'filename',
+        file_status = true, -- displays file status (readonly status, modified status)
+        path = 0, -- 0 = just filename, 1 = relative path, 2 = absolute path
+      }    },
+    lualine_x = {
+      {
+        'diagnostics',
+        sources = { 'nvim_diagnostic' },
+        symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
+      },
+      'encoding',
+      'filetype',
+    },
     lualine_y = { 'progress' },
     lualine_z = { 'location' },
   },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { 'filename' },
-    lualine_x = { 'location' },
-    lualine_y = {},
-    lualine_z = {},
-  },
-  tabline = {},
-  extensions = {},
+  -- inactive_sections = {
+  --   lualine_a = {},
+  --   lualine_b = {},
+  --   lualine_c = { {
+  --     'filename',
+  --     file_status = true, -- displays file status (readonly status, modified status)
+  --     path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+  --   } },
+  --   lualine_x = { 'location' },
+  --   lualine_y = {},
+  --   lualine_z = {}
+  -- },
 }
