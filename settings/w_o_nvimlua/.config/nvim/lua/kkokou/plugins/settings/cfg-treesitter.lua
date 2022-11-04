b1a21a5fd30e9e30539d133a86cd7ace4ab54fe3
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2022-11-04 11:54:24 -0400

    Remove some treesitter plugins
    nvim-treesitter/nvim-treesitter-refactor
    nvim-treesitter/nvim-treesitter-textobjects

--- settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/settings/cfg-treesitter.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/settings/cfg-treesitter.lua
@@ -19,15 +19,14 @@ require('nvim-treesitter.configs').setup {
     'tsx',
     'typescript',
     'yaml',
-  }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  -- ignore_install = { "javascript" }, -- List of parsers to ignore installing
+  },
+
+  sync_install = false,
+
+  auto_install = true,
+
   highlight = {
-    enable = true, -- false will disable the whole extension
-    -- disable = { "c", "rust" },  -- list of language that will be disabled
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
+    enable = true,
     additional_vim_regex_highlighting = false,
   },
 
@@ -54,79 +53,4 @@ require('nvim-treesitter.configs').setup {
     enable_autocmd = false,
   },
 
-  refactor = {
-    highlight_definitions = { enable = true },
-    highlight_current_scope = { enable = false },
-    smart_rename = {
-      enable = true,
-      keymaps = {
-        smart_rename = 'grr',
-      },
-    },
-
-    navigation = {
-      enable = true,
-      keymaps = {
-        goto_definition = 'gnd',
-        list_definitions = 'gnD',
-        list_definitions_toc = 'gO',
-        goto_next_usage = '<a-*>',
-        goto_previous_usage = '<a-#>',
-      },
-    },
-  },
-
-  textobjects = {
-    select = {
-      enable = true,
-
-      -- Automatically jump forward to textobj, similar to targets.vim
-      lookahead = true,
-
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      },
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ['<leader>a'] = '@parameter.inner',
-      },
-      swap_previous = {
-        ['<leader>A'] = '@parameter.inner',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
-      },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
-      },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
-      },
-    },
-    lsp_interop = {
-      enable = true,
-      border = 'none',
-      peek_definition_code = {
-        ['<leader>df'] = '@function.outer',
-        ['<leader>dF'] = '@class.outer',
-      },
-    },
-  },
 }
