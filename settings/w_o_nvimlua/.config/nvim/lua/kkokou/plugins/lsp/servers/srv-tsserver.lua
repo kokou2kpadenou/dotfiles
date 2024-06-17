6dfd1ec93ab084f6164ff5a67880591b83101b68
Kokou Kpadenou <kkokou@gmail.com>
2024-06-17 12:58:39 -0400

    clean tsserver lsp config

--- settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/lsp/servers/srv-tsserver.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/lsp/servers/srv-tsserver.lua
@@ -1,11 +1,5 @@
 return function()
   return {
-
-    -- TODO: Check if it is ok to remove this on_attach to disable formating <kkokou>
-    -- on_attach = function(client, bufnr)
-    --   client.server_capabilities.document_formatting = false
-    --   client.server_capabilities.document_range_formatting = false
-    -- end,
     -- taken from https://github.com/typescript-language-server/typescript-language-server#workspacedidchangeconfiguration
     disable_formatting = true,
     settings = {
