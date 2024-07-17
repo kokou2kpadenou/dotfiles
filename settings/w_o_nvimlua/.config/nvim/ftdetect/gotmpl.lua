64deee0a492af677d06f6e10bb80991d19335ac8
Kokou Kpadenou <kkokou@gmail.com>
2024-07-17 00:35:17 -0400

    add more go template extension pattern

--- settings/w_o_nvimlua/.config/nvim/ftdetect/gotmpl.lua
+++ settings/w_o_nvimlua/.config/nvim/ftdetect/gotmpl.lua
@@ -2,10 +2,9 @@ vim.api.nvim_create_autocmd({
   'BufNewFile',
   'BufRead',
 }, {
-  pattern = '*.yaml,*.yml,*.html',
+  pattern = '*.yaml,*.yml,*.html,*.gotmpl,*.gotexttmpl,*.gohtmltmpl,*.gotext,*.gohtml',
   callback = function()
     if vim.fn.search('{{.\\+}}', 'nw') ~= 0 then
-      -- local buf = vim.api.nvim_get_current_buf()
       vim.api.nvim_set_option_value('filetype', 'gotmpl', { buf = 0 })
     end
   end,
