fbaf2c203fdb0647af862d0f5ea561ba8e3d1206
Kokou Kpadenou <kkokou@gmail.com>
2024-08-02 13:30:19 -0400

    improve conform settings

--- settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/formatter.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/formatter.lua
@@ -2,7 +2,9 @@
 return {
   {
     'stevearc/conform.nvim',
-    lazy = false,
+    -- lazy = false,
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
     keys = {
       {
         '<space>f',
@@ -13,23 +15,55 @@ return {
         desc = '[F]ormat buffer',
       },
     },
+    -- This will provide type hinting with LuaLS
+    ---@module "conform"
+    ---@type conform.setupOpts
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
         local disable_filetypes = { c = true, cpp = true }
+
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
         }
       end,
+
       formatters_by_ft = {
         lua = { 'stylua' },
-        javascript = { { 'prettierd', 'prettier' } },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        python = { 'isort', 'black' },
+        go = {
+          'goimports',
+          'gofmt' --[[ , 'injected' ]],
+        },
+        gotmpl = { 'prettierd', 'prettier', stop_after_first = true },
+        sql = { 'sql_formatter' },
       },
+
+      -- formatters = {
+      --   injected = {
+      --     -- Set the options field
+      --     options = {
+      --       -- Set individual option values
+      --       ignore_errors = true,
+      --       lang_to_formatters = {
+      --         sql = { 'sql_formatter' },
+      --       },
+      --     },
+      --   },
+      -- },
     },
     init = function()
       -- formatexpr; gq
       vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+      -- disable autoformat initially
+      vim.g.disable_autoformat = true
     end,
   },
 }
