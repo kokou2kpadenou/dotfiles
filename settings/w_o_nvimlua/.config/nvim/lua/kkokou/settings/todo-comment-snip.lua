b4748404b8ac8090552d54e491880e641df70f8b
Kokou Kpadenou <kkokou@gmail.com>
2024-08-02 14:10:30 -0400

    reform the code

--- settings/w_o_nvimlua/.config/nvim/lua/kkokou/settings/todo-comment-snip.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/kkokou/settings/todo-comment-snip.lua
@@ -59,7 +59,7 @@ local marks = {
 
 local todo_snippet_nodes = function(aliases, opts)
   local aliases_nodes = vim.tbl_map(function(alias)
-    return i(nil, alias)   -- generate choices for [name-of-comment]
+    return i(nil, alias) -- generate choices for [name-of-comment]
   end, aliases)
   local sigmark_nodes = {} -- choices for [comment-mark]
   for _, mark in pairs(marks) do
@@ -70,9 +70,9 @@ local todo_snippet_nodes = function(aliases, opts)
     f(function()
       return get_cstring(opts.ctype)[1] -- get <comment-string[1]>
     end),
-    c(1, aliases_nodes),                -- [name-of-comment]
-    i(3),                               -- {comment-text}
-    c(2, sigmark_nodes),                -- [comment-mark]
+    c(1, aliases_nodes), -- [name-of-comment]
+    i(3), -- {comment-text}
+    c(2, sigmark_nodes), -- [comment-mark]
     f(function()
       return get_cstring(opts.ctype)[2] -- get <comment-string[2]>
     end),
@@ -87,42 +87,39 @@ end
 ---@param opts table merged with the snippet opts table
 local todo_snippet = function(context, aliases, opts)
   opts = opts or {}
-  aliases = type(aliases) == 'string' and { aliases } or
-  aliases                                                        -- if we do not have aliases, be smart about the function parameters
+  aliases = type(aliases) == 'string' and { aliases } or aliases -- if we do not have aliases, be smart about the function parameters
   context = context or {}
   if not context.trig then
-    return error("context doesn't include a `trig` key which is mandatory", 2)                 -- all we need from the context is the trigger
+    return error("context doesn't include a `trig` key which is mandatory", 2) -- all we need from the context is the trigger
   end
-  opts.ctype = opts.ctype or
-  1                                                                                            -- comment type can be passed in the `opts` table, but if it is not, we have to ensure, it is defined
-  local alias_string = table.concat(aliases, '|')                                              -- `choice_node` documentation
-  context.name = context.name or (alias_string .. ' comment')                                  -- generate the `name` of the snippet if not defined
-  context.dscr = context.dscr or (alias_string .. ' comment with a signature-mark')            -- generate the `dscr` if not defined
-  context.docstring = context.docstring or
-  (' {1:' .. alias_string .. '}: {3} <{2:mark}>{0} ')                                          -- generate the `docstring` if not defined
-  local comment_node = todo_snippet_nodes(aliases, opts)                                       -- nodes from the previously defined function for their generation
-  return s(context, comment_node, opts)                                                        -- the final todo-snippet constructed from our parameters
+  opts.ctype = opts.ctype or 1 -- comment type can be passed in the `opts` table, but if it is not, we have to ensure, it is defined
+  local alias_string = table.concat(aliases, '|') -- `choice_node` documentation
+  context.name = context.name or (alias_string .. ' comment') -- generate the `name` of the snippet if not defined
+  context.dscr = context.dscr or (alias_string .. ' comment with a signature-mark') -- generate the `dscr` if not defined
+  context.docstring = context.docstring or (' {1:' .. alias_string .. '}: {3} <{2:mark}>{0} ') -- generate the `docstring` if not defined
+  local comment_node = todo_snippet_nodes(aliases, opts) -- nodes from the previously defined function for their generation
+  return s(context, comment_node, opts) -- the final todo-snippet constructed from our parameters
 end
 
 local todo_snippet_specs = {
-  { { trig = 'todo' },  'TODO' },
-  { { trig = 'fix' },   { 'FIX', 'BUG', 'ISSUE', 'FIXIT' } },
-  { { trig = 'hack' },  'HACK' },
-  { { trig = 'warn' },  { 'WARN', 'WARNING', 'XXX' } },
-  { { trig = 'perf' },  { 'PERF', 'PERFORMANCE', 'OPTIM', 'OPTIMIZE' } },
-  { { trig = 'note' },  { 'NOTE', 'INFO' } },
-  -- NOTE: Block commented todo-comments <kunzaatko>
-  { { trig = 'todob' }, 'TODO',                                         { ctype = 2 } },
-  { { trig = 'fixb' },  { 'FIX', 'BUG', 'ISSUE', 'FIXIT' },             { ctype = 2 } },
-  { { trig = 'hackb' }, 'HACK',                                         { ctype = 2 } },
-  { { trig = 'warnb' }, { 'WARN', 'WARNING', 'XXX' },                   { ctype = 2 } },
+  { { trig = 'todo' }, 'TODO' },
+  { { trig = 'fix' }, { 'FIX', 'BUG', 'ISSUE', 'FIXIT' } },
+  { { trig = 'hack' }, 'HACK' },
+  { { trig = 'warn' }, { 'WARN', 'WARNING', 'XXX' } },
+  { { trig = 'perf' }, { 'PERF', 'PERFORMANCE', 'OPTIM', 'OPTIMIZE' } },
+  { { trig = 'note' }, { 'NOTE', 'INFO' } },
+  -- Block commented todo-comments <kunzaatko>
+  { { trig = 'todob' }, 'TODO', { ctype = 2 } },
+  { { trig = 'fixb' }, { 'FIX', 'BUG', 'ISSUE', 'FIXIT' }, { ctype = 2 } },
+  { { trig = 'hackb' }, 'HACK', { ctype = 2 } },
+  { { trig = 'warnb' }, { 'WARN', 'WARNING', 'XXX' }, { ctype = 2 } },
   { { trig = 'perfb' }, { 'PERF', 'PERFORMANCE', 'OPTIM', 'OPTIMIZE' }, { ctype = 2 } },
-  { { trig = 'noteb' }, { 'NOTE', 'INFO' },                             { ctype = 2 } },
+  { { trig = 'noteb' }, { 'NOTE', 'INFO' }, { ctype = 2 } },
 }
 
 local todo_comment_snippets = {}
 for _, v in ipairs(todo_snippet_specs) do
-  -- NOTE: 3rd argument accepts nil
+  -- 3rd argument accepts nil
   table.insert(todo_comment_snippets, todo_snippet(v[1], v[2], v[3]))
 end
 
