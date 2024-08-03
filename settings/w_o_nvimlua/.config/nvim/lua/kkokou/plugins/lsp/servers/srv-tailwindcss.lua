d2b261a86b7fe1c6e4da41b429a9907f3476ad0d
Kokou Kpadenou <kkokou@gmail.com>
2024-08-03 16:38:03 -0400

    allow tailwindcss to support gotmpl

---
+++ settings/w_o_nvimlua/.config/nvim/lua/kkokou/plugins/lsp/servers/srv-tailwindcss.lua
@@ -0,0 +1,12 @@
+return function()
+  return {
+    settings = {
+      tailwindCSS = {
+        includeLanguages = {
+          gotmpl = 'html',
+        },
+        emmetCompletions = true,
+      },
+    },
+  }
+end
