97ca5a9381a8a558c24a8c3cac6e4a6c98e4097b
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2025-07-28 18:06:47 -0400

    update docker install script to support bluidx

--- scripts/archlinux/docker_install.sh
+++ scripts/archlinux/docker_install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-sudo pacman -Syu docker          # Install Docker
-sudo pacman -Syu docker-compose  # Install docker-compose
+# Packages installation
+sudo pacman -Syu docker docker-compose docker-buildx
 
 # Start Docker on system startup
-sudo systemctl enable --now docker
+sudo systemctl enable --now docker.service
 
 # Add your user to the `docker` group so you can use Docker without sudo
 sudo usermod -a -G docker $(whoami)
+
+# create group docker
+sudo newgrp docker
