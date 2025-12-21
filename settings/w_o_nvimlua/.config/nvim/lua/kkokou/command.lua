090a0dd9ed6f9ea84511aaba27772d006fea9636
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2025-12-20 23:05:16 -0500

    reorganize the config

--- settings/w_o_nvimlua/.config/nvim/lua/kkokou/command.lua
+++ settings/w_o_nvimlua/.config/nvim/lua/kkokou/command.lua
@@ -9,6 +9,123 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- restore cursor to file position in previous editing session
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function(args)
+		local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+		local line_count = vim.api.nvim_buf_line_count(args.buf)
+		if mark[1] > 0 and mark[1] <= line_count then
+			vim.api.nvim_win_set_cursor(0, mark)
+			-- defer centering slightly so it's applied after render
+			vim.schedule(function()
+				vim.cmd("normal! zz")
+			end)
+		end
+	end,
+})
+
+-- syntax highlighting for dotenv files
+vim.api.nvim_create_autocmd("BufRead", {
+	group = vim.api.nvim_create_augroup("dotenv_ft", { clear = true }),
+	pattern = { ".env", ".env.*" },
+	callback = function()
+		vim.bo.filetype = "dosini"
+	end,
+})
+
+-- show cursorline only in active window enable
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+	group = vim.api.nvim_create_augroup("active_cursorline", { clear = true }),
+	callback = function()
+		vim.opt_local.cursorline = true
+	end,
+})
+
+-- show cursorline only in active window disable
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+	group = "active_cursorline",
+	callback = function()
+		vim.opt_local.cursorline = false
+	end,
+})
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    -- vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = function(desc)
+      local curr_opts = { buffer = ev.buf }
+      if desc then
+        curr_opts.desc = 'LSP: ' .. desc
+      end
+      return curr_opts
+    end
+
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts())
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts())
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts())
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts())
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts())
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts())
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts())
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts())
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts())
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts())
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts())
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts())
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts())
+
+    -- The following two autocommands are used to highlight references of the
+    -- word under your cursor when your cursor rests there for a little while.
+    --    See `:help CursorHold` for information about when this is executed
+    --
+    -- When you move your cursor, the highlights will be cleared (the second autocommand).
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client and client.server_capabilities.documentHighlightProvider then
+      local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        buffer = ev.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+        buffer = ev.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.clear_references,
+      })
+
+      vim.api.nvim_create_autocmd('LspDetach', {
+        group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+        callback = function(event2)
+          vim.lsp.buf.clear_references()
+          vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+        end,
+      })
+    end
+
+    -- The following autocommand is used to enable inlay hints in your
+    -- code, if the language server you are using supports them
+    --
+    -- This may be unwanted, since they displace some of your code
+    if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+      vim.keymap.set('n', '<space>th', function()
+        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = 0 })
+      end, { buffer = ev.buf, desc = 'LSP: [T]oggle Inlay [H]ints' })
+    end
+  end,
+})
+
 -- Comfort format command
 vim.api.nvim_create_user_command('Format', function(args)
   local range = nil
